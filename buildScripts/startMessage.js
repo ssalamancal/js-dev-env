07b40b005878007f29991e278c3019c6c0536e60
Santiago Salamanca L <s.salamanca.lopez@hotmail.com>
2017-10-24 11:55:15 -0500

    Crear archivo de configuracion de ESLint, usar configuracion recomendada de ESLint y sobre escribir regla de no-console y volverlo un warning
    En el package.json agrgar script para aplicar reglas de eslint a el archivo de configuracion webpack.config.* a la carpeta src y builScripts, adicionamente crear script para que eslint se ejecute como vigilante y esto se haga en el momento de levantar el servidor (comando start)
    
    Excluir lineas de codigo con comentarios que eslint entiende

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,4 +1,4 @@
 //var chalk = require('chalk');
 import chalk from 'chalk'; // ES6 Code version, ir a start script y modificar node por babel-node
 
-console.log(chalk.green('Starting app in dev mode...'));
+console.log(chalk.green('Starting app in dev mode...')); // eslint-disable-line no-console
